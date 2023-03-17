b1939ae1f9779244ff40767492ec2d1991a61017
eyad <eyadamro905@gmail.com>
2023-03-17 16:16:07 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+int n = 65;
+for(n = 65; n < 123;n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}	
